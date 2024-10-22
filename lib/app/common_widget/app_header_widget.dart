afb72040db52879161f94d26747165c9cfebf3ef
jatinkumarsahoo <jatnkumarsahoo99@gmail.com>
2024-10-22 18:38:30 +0530

    registration api integration

--- lib/app/common_widget/app_header_widget.dart
+++ lib/app/common_widget/app_header_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppHeaderWidget extends StatelessWidget {
-  const AppHeaderWidget({super.key});
+  final double? height;
+  final double? imageOneWidth;
+  final double? imageTwoWidth;
+  const AppHeaderWidget({super.key, this.height, this.imageOneWidth, this.imageTwoWidth});
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +12,12 @@ class AppHeaderWidget extends StatelessWidget {
     return  Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 3),
       child: SizedBox(
-        height:size.height*0.2 ,
+        height:height?? size.height*0.2 ,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Image.asset("assets/images/logo.png",width: size.width*0.5,),
-            Image.asset("assets/images/got_logo.png",width: size.width*0.25,),
+            Image.asset("assets/images/logo.png",width:imageOneWidth?? size.width*0.5,),
+            Image.asset("assets/images/got_logo.png",width: imageTwoWidth??size.width*0.25,),
           ],
         ),
       ),
--- lib/app/module/register_screen/register_screen.dart
+++ lib/app/module/register_screen/register_screen.dart
@@ -16,101 +16,198 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController fullNameTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: AppBackgroundScreen(child: [
-        SingleChildScrollView(
-          reverse: true,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const AppHeaderWidget(),
-              SizedBox(
-                height: size.height*0.1,
-              ),
-              Text("Welcome to Onboard!",style: TextStyles(context).googlePoppinsFontsForText(fontSize: 18,fontWeight: FontWeight.w700,color: const Color(0xFF000000)),),
-               SizedBox(
-                height: AppConstant.heightBetweenWidget,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 85.0),
-                child: SizedBox(
-                    child: Text("RTI Platform help citizens to file your RTI Application digitally.",style: TextStyles(context).googlePoppinsFontsForText(fontSize: 13,fontWeight: FontWeight.w400,color: const Color.fromRGBO(0, 0, 0, 0.8)),textAlign: TextAlign.center,)),
-              ),
-               SizedBox(
-                height: AppConstant.heightBetweenWidget,
-              ),
-              AppTextField(
-                controller: TextEditingController(),
-                // errorText: _errorFName,
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                titleText: "Enter your full name",
-                hintText: "Enter your full name",
-                keyboardType: TextInputType.name,
-                radius: 100,
-                onChanged: (String txt) {},
-              ),
-               SizedBox(
-                height: AppConstant.heightBetweenWidget,
-              ),
-              AppTextField(
-                controller: TextEditingController(),
-                // errorText: _errorFName,
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                titleText: "Enter your Email",
-                hintText: "Enter your Email",
-                keyboardType: TextInputType.name,
-                radius: 100,
-                onChanged: (String txt) {},
-              ),
-               SizedBox(
-                height: AppConstant.heightBetweenWidget,
-              ),
-              AppTextField(
-                controller: TextEditingController(),
-                // errorText: _errorFName,
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                titleText: "Enter Password",
-                hintText: "Enter Password",
-                keyboardType: TextInputType.name,
-                radius: 100,
-                onChanged: (String txt) {},
-              ),
-               SizedBox(
-                height: AppConstant.heightBetweenWidget,
-              ),
-              AppTextField(
-                controller: TextEditingController(),
-                // errorText: _errorFName,
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                titleText: "Confirm Password",
-                hintText: "Confirm Password",
-                keyboardType: TextInputType.name,
-                radius: 100,
-                onChanged: (String txt) {},
-              ),
-               SizedBox(
-                height: AppConstant.heightBetweenWidget+10,
-              ),
-
-              CommonButton(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                buttonText: "Register",
-                onTap: () {
-                  Navigator.pushNamed(context, "/logInScreen");
-                },
-              ),
-              SizedBox(
-                height: AppConstant.heightBetweenWidget,
-              ),
-              FooterTextWidget(onTap: () {  },highlightedText: "Sign In",text: "Already have an account ? ",)
-            ],
-          ),
-        )
-      ],),
+      body: AppBackgroundScreen(
+        child: [
+          SingleChildScrollView(
+            reverse: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                 AppHeaderWidget(height: size.height*0.05,imageOneWidth: size.width*0.2,imageTwoWidth: size.width*0.1,),
+                Text(
+                  "Welcome to Onboard!",
+                  style: TextStyles(context)
+                      .googlePoppinsFontsForText(fontSize: 12, fontWeight: FontWeight.w700, color: const Color(0xFF000000)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 85.0),
+                  child: SizedBox(
+                      child: Text(
+                    "RTI Platform help citizens to file your RTI Application digitally.",
+                    style: TextStyles(context).googlePoppinsFontsForText(
+                        fontSize: 10, fontWeight: FontWeight.w400, color: const Color.fromRGBO(0, 0, 0, 0.8)),
+                    textAlign: TextAlign.center,
+                  )),
+                ),
+                SizedBox(
+                  height: AppConstant.heightBetweenWidget -9,
+                ),
+                SizedBox(
+                  height: size.height*0.65,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        AppTextField(
+                          controller: fullNameTextEditingController,
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Enter your full name",
+                          hintText: "Enter your full name",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Enter your Email",
+                          hintText: "Enter your Email",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Enter Password",
+                          hintText: "Enter Password",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Confirm Password",
+                          hintText: "Confirm Password",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget ,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Enter your full name",
+                          hintText: "Enter your full name",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Enter your Email",
+                          hintText: "Enter your Email",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Enter Password",
+                          hintText: "Enter Password",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Confirm Password",
+                          hintText: "Confirm Password",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget ,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Enter Password",
+                          hintText: "Enter Password",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget,
+                        ),
+                        AppTextField(
+                          controller: TextEditingController(),
+                          // errorText: _errorFName,
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          titleText: "Confirm Password",
+                          hintText: "Confirm Password",
+                          keyboardType: TextInputType.name,
+                          radius: 100,
+                          onChanged: (String txt) {},
+                        ),
+                        SizedBox(
+                          height: AppConstant.heightBetweenWidget +20 ,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                CommonButton(
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                  buttonText: "Register",
+                  onTap: () {
+                    Navigator.pushNamed(context, "/logInScreen");
+                  },
+                ),
+                SizedBox(
+                  height: AppConstant.heightBetweenWidget,
+                ),
+                FooterTextWidget(
+                  onTap: () {},
+                  highlightedText: "Sign In",
+                  text: "Already have an account ? ",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
